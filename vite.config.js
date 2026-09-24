42d244541b3787df04c6b31a6dadc332324a50e9
Miguel Solorio <miguel.solorio07@gmail.com>
2026-09-23 23:15:56 -0700

    Start over

--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  build: {
-    // three.js alone is ~650 kB minified; one chunk is fine for a game.
-    chunkSizeWarningLimit: 1000,
-  },
-});
